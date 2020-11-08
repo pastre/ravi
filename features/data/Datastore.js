9ba68d8c784b5d470d8763f1fe1f6a8bc87861d0
pastre <pastre68@gmail.com>
2020-11-08 16:48:58 -0300

    reorganized file and class  structures

--- features/data/Datastore.js
+++ features/data/Datastore.js
@@ -1,22 +1,4 @@
-class AbstractDatastoreBackend {
-	getEvents = () => { throw "did not implement getEvents" }
-	getSubscriptions = () => { throw "did not implement getSubscriptions" }
-	addSubscription = (event) => { throw "did not implement addSubscription" }
-	getEventByName = (eventName) => { throw "did not implement getEventByName" }
-}
-
-class MockedDatastoreBackend {
-	constructor() {
-		this.events = ["get_together", "apple_talk"]
-		this.subscriptions = ['event']
-	}
-
-	getEvents = () => this.events
-	getEventByName = (eventName) => this.events.find(event => eventName === event)
-	getSubscriptions = () => this.subscriptions
-	addSubscription(event) { this.subscriptions.push(event) }
-}
-
+const MockedDatastoreBackend = require('./backends/MockedDatastoreBackend')
 let datastoreBackend = new MockedDatastoreBackend()
 
 getEvents = () => datastoreBackend.getEvents()
